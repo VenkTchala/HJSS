418d7d9152daca4d41f9ab89e9ac58ede70ed033
Code breaker <code@breaker.com>
2024-03-10 16:45:29 +0530

    Added Methods to get Lessons in timetable by Coach, Day of week, Grade.

--- src/main/java/com/example/HJSS/Timetable.java
+++ src/main/java/com/example/HJSS/Timetable.java
@@ -1,7 +1,28 @@
 package com.example.HJSS;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
 public class Timetable {
     private List<Lesson> lessons;
+
+    public List<Lesson> getLessonsByDay(DayOfWeek week) {
+        return lessons.stream()
+                .filter(i -> i.getDate().getDayOfWeek().equals(week))
+                .toList();
+    }
+
+    public List<Lesson> getLessonsByCoach(Coach coach){
+        return lessons.stream()
+                .filter(i -> i.getCoach().equals(coach))
+                .toList();
+    }
+
+    public List<Lesson> getLessonsByGrade(GradeLevel level){
+        return lessons.stream()
+                .filter(i -> i.getGradeLevel().equals(level))
+                .toList();
+    }
+
+
 }
